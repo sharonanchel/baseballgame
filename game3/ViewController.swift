0b17c0655c60078d6839b8c5af16f13f6b9fd867
cuauhtemoch <mvcoco650@gmail.com>
2017-09-09 11:09:51 -0700

    first commit

--- game3/ViewController.swift
+++ game3/ViewController.swift
@@ -9,17 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var countdownLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    @IBOutlet weak var StartButton: UIButton!
+  
+    var countdownTimer: Timer!
+    var totalTime = 3
+    
+    func startTimer() {
+        countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    }
+    
+    func updateTime() {
+        countdownLabel.text = "\(timeFormatted(totalTime))"
+        
+        if totalTime != 0 {
+            totalTime -= 1
+        } else {
+            endTimer()
+            countdownLabel.text = "GO"
+        }
+    }
+    
+    
+    func endTimer() {
+        countdownTimer.invalidate()
+    }
+    
+    func timeFormatted(_ totalSeconds: Int) -> String {
+        let seconds: Int = totalSeconds % 60
+        //        let minutes: Int = (totalSeconds / 60) % 60
+        //     let hours: Int = totalSeconds / 3600
+        return String(format: "%2d", seconds)
+    }
+    
+    @IBAction func startButton(_ sender: UIButton) {
+        
+        startTimer()
+        
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
+
 
