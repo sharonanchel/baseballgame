06493aa6e898247bb728db1c19353c2e39e5d10b
cuauhtemoch <mvcoco650@gmail.com>
2017-09-09 17:42:48 -0700

    Change lines for leaderboard

--- game3/ViewController.swift
+++ game3/ViewController.swift
@@ -144,6 +144,7 @@ class ViewController: UIViewController {
             resetGame()
         } else if hits == 1 {
             countdownLabel.text = "The batter hit the ball, You Lose"
+            
             resetGame()
         }
     }
