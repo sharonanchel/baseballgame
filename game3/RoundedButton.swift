7fa9026cc23c258c2487ffe73f0178048fae051e
sharonanchel <sharonanchel@sharons-macbook-pro-2.local>
2017-09-11 08:43:52 -0700

    Changed some color

---
+++ game3/RoundedButton.swift
@@ -0,0 +1,25 @@
+//
+//  RoundedButton.swift
+//  game3
+//
+//  Created by Sharon Anchel on 9/10/17.
+//  Copyright © 2017 TheGroup1. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderWidth = 1/UIScreen.main.nativeScale
+        contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        titleLabel?.adjustsFontForContentSizeCategory = true
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = frame.height/2
+        layer.borderColor = isEnabled ? tintColor.cgColor : UIColor.orange.cgColor
+    }
+}
